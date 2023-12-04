fe7babe6bb0adbec2db11f852b1ac99146d62234
git-leon <xleonhunter@gmail.com>
2023-12-04 18:46:04 -0500

    recursive attempt

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -1,6 +1,9 @@
 package com.github.curriculeon;
 
+import java.util.Arrays;
+
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(SumFirstTwo.sum(1,2,3,4)));
     }
 }
--- src/main/java/com/github/curriculeon/SumFirstTwo.java
+++ src/main/java/com/github/curriculeon/SumFirstTwo.java
@@ -1,7 +1,26 @@
+//[1,2,3,4]
+// get the sum = 3
+//[2,3,4] >>> trim first element
+//[3,3,4] >>> set sum to first element
 package com.github.curriculeon;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class SumFirstTwo {
-    public static int[] sum(int[] array) {
-        return null;
+    public static int[] sum(int... array) {
+        List<Integer> list = new ArrayList<>();
+        if(array.length > 1) {
+            int firstValue = array[0];
+            int secondValue = array[1];
+            int sum = firstValue + secondValue;
+            int[] newArray = Arrays.copyOfRange(array, 1, array.length);
+            newArray[0] = sum;
+            int arraySum = IntStream.of(sum(newArray)).sum();
+            list.add(arraySum);
+        }
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
