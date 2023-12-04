9d6dc17f77bd107b9f0172b355228881f0819c75
git-leon <xleonhunter@gmail.com>
2023-12-04 18:27:01 -0500

    updated project structure

---
+++ src/main/java/com/github/curriculeon/NextPermutation.java
@@ -0,0 +1,7 @@
+package com.github.curriculeon;
+
+public class NextPermutation {
+    public static String nextPermutation(String word) {
+        return null;
+    }
+}
--- src/main/java/com/github/curriculeon/SumFirstTwo.java
+++ src/main/java/com/github/curriculeon/SumFirstTwo.java
@@ -1,3 +1,5 @@
+package com.github.curriculeon;
+
 public class SumFirstTwo {
     public static int[] sum(int[] array) {
         return null;
